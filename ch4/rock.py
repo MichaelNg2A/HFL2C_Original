7472b71656947fcacb191f52c400c43f964fe81f
Michael Ng <MichaelNg2A@users.noreply.github.com>
2018-02-17 19:49:42 -0800

    Made output objects instead of numbers

--- ch4/rock.py
+++ ch4/rock.py
@@ -4,4 +4,16 @@ import random
 
 random_choice = random.randint(0,2)
 
-print ('The computer chooses', random_choice)
+if random_choice == 0:
+	
+	computer_choice = 'rock'
+	
+elif random_choice == 1:
+	
+	computer_choice = 'paper'
+	
+else:
+	
+	computer_choice = 'scissors'
+
+print ('The computer chooses', computer_choice)
