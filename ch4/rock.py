2c811dc89dd93341c6739d461f2a12f7911c2cc3
Michael Ng <MichaelNg2A@users.noreply.github.com>
2018-02-21 09:35:31 -0800

    Added test for user input

--- ch4/rock.py
+++ ch4/rock.py
@@ -8,7 +8,11 @@ choices = ['rock', 'paper', 'scissors']
 
 computer_choice = random.choice(choices)
 
-user_choice = input('rock, paper or scissors? ')
+user_choice = ''
+while (user_choice != 'rock' and
+    user_choice != 'paper' and
+    user_choice != 'scissors'):
+    	user_choice = input('rock, paper or scissors? ')
 
 print('You choose', user_choice, 'and the computer choose', computer_choice)
 
