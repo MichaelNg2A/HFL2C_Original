293fae219524f080e169e9ea4c1b7d1dfdd4a612
Michael Ng <MichaelNg2A@users.noreply.github.com>
2018-02-20 20:19:59 -0800

    Finished rock.py

--- ch4/rock.py
+++ ch4/rock.py
@@ -2,6 +2,8 @@
 
 import random
 
+winner = ''
+
 choices = ['rock', 'paper', 'scissors']
 
 computer_choice = random.choice(choices)
@@ -9,3 +11,24 @@ computer_choice = random.choice(choices)
 user_choice = input('rock, paper or scissors? ')
 
 print('You choose', user_choice, 'and the computer choose', computer_choice)
+
+if computer_choice == user_choice:
+	winner = 'Tie'
+
+elif computer_choice == 'paper' and user_choice == 'rock':
+	winner = 'Computer'
+
+elif computer_choice == 'rock' and user_choice == 'scissors':
+	winner = 'Computer'
+
+elif computer_choice == 'scissors' and user_choice == 'paper':
+	winner = 'Computer'
+
+else:
+	winner = 'User'
+
+if winner == 'Tie':
+	print('We both choose', computer_choice + ', play again.')
+
+else:
+	print(winner, 'won, I choose', computer_choice + '.')
