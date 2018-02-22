d313330701e071f3cdfa264c7415b9b3883643b3
Michael Ng <MichaelNg2A@users.noreply.github.com>
2018-02-21 19:58:55 -0800

    Cleaned up Indents
    Found that the default indent in Pythonista is 2 spaces.
    
    This commit is to change the indents in my existing programs to the
    Python standard of 4 spaces.

--- ch2/phraseomatic.py
+++ ch2/phraseomatic.py
@@ -1,17 +1,17 @@
 import random
 
 verbs = ['Leverage', 'Sync', 'Target',
-         'Gamify', 'Offline', 'Crowd-sourced',
-         '24/7', 'Lean-in', '30,000 foot']
+    'Gamify', 'Offline', 'Crowd-sourced',
+    '24/7', 'Lean-in', '30,000 foot']
 
 adjectives = ['A/B Tested', 'Freemium',
-              'Hyperlocal', 'Siloed', 'B-to-B',
-              'Oriented', 'Cloud-based', 
-              'API-based']
+    'Hyperlocal', 'Siloed', 'B-to-B',
+    'Oriented', 'Cloud-based', 
+    'API-based']
 
 nouns = ['Early Adopter', 'Low-hanging Fruit',
-         'Pipeline', 'Splash Page', 'Productivity',
-         'Process', 'Tipping Point', 'Paradigm']
+    'Pipeline', 'Splash Page', 'Productivity',
+    'Process', 'Tipping Point', 'Paradigm']
 
 verb = random.choice(verbs)
 adjective = random.choice(adjectives)
