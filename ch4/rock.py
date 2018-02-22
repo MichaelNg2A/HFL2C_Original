1afadc513cffb91b83275665209b907b12e167d5
Michael Ng <MichaelNg2A@users.noreply.github.com>
2018-02-21 19:58:55 -0800

    Cleaned up Indents
    Found that the default indent in Pythonista is 2 spaces.
    
    This commit is to change the indents in my existing programs to the
    Python standard of 4 spaces.

--- ch4/rock.py
+++ ch4/rock.py
@@ -12,27 +12,27 @@ user_choice = ''
 while (user_choice != 'rock' and
     user_choice != 'paper' and
     user_choice != 'scissors'):
-    	user_choice = input('rock, paper or scissors? ')
+        user_choice = input('rock, paper or scissors? ')
 
 print('You choose', user_choice, 'and the computer choose', computer_choice)
 
 if computer_choice == user_choice:
-	winner = 'Tie'
+    winner = 'Tie'
 
 elif computer_choice == 'paper' and user_choice == 'rock':
-	winner = 'Computer'
+    winner = 'Computer'
 
 elif computer_choice == 'rock' and user_choice == 'scissors':
-	winner = 'Computer'
+    winner = 'Computer'
 
 elif computer_choice == 'scissors' and user_choice == 'paper':
-	winner = 'Computer'
+    winner = 'Computer'
 
 else:
-	winner = 'User'
+    winner = 'User'
 
 if winner == 'Tie':
-	print('We both choose', computer_choice + ', play again.')
+    print('We both choose', computer_choice + ', play again.')
 
 else:
-	print(winner, 'won, I choose', computer_choice + '.')
+    print(winner, 'won, I choose', computer_choice + '.')
