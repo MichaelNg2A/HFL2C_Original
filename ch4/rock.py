10e2c9d70e48b2266929a50b3393bbce868c5318
Michael Ng <michael.ng@michaelng-wsm.internal.salesforce.com>
2018-02-22 18:51:58 -0800

    Added More Comments and a nice header to rock.py

--- ch4/rock.py
+++ ch4/rock.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+############################################################
+# rock.py                                                  #
+############################################################
+#                                                          #
+# Name:       rock.py                                      #
+# Version:    1.0                                          #
+# Updated February 22nd, 2018                              #
+# Author:     MichaelNg2A                                  #
+#                                                          #
+# This is a Python Program to play Rock, Paper Scissors    #
+# with the computer.  Based on the exercises in Head First #
+# Learn to Code - Chapter #3.  Good Luck!                  #
+#                                                          #
+############################################################
 
 import random
 
@@ -22,10 +36,16 @@ while (user_choice != 'rock' and
     user_choice != 'scissors'):
         user_choice = input('rock, paper or scissors? ')
 
+# Display what everyone's choices were.
+
 print('You choose', user_choice, 'and the computer choose', computer_choice)
+
+#  Define the "winner" if there's a tie.
 
 if computer_choice == user_choice:
     winner = 'Tie'
+
+# elif tree to walk through all the other scenarios where the Computer wins.
 
 elif computer_choice == 'paper' and user_choice == 'rock':
     winner = 'Computer'
@@ -36,11 +56,17 @@ elif computer_choice == 'rock' and user_choice == 'scissors':
 elif computer_choice == 'scissors' and user_choice == 'paper':
     winner = 'Computer'
 
+# If the computer is not the winner, the User must be the winner.
+
 else:
     winner = 'User'
 
+# Use this wording on the winner line if there's a tie.
+
 if winner == 'Tie':
     print('We both choose', computer_choice + ', play again.')
+
+# If there's only one winner, use the original wording to announce it.
 
 else:
     print(winner, 'won, I choose', computer_choice + '.')
