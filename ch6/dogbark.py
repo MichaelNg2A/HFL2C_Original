aeb0c0386147e615f81fdbbc9792e54d56fd88f5
Michael Ng <MichaelNg.GitHub@KWMeads.com>
2018-03-16 20:17:51 -0700

    Created bark function
    Created bark function and folded into while loop.
    
    Not exactly what the book said to do yet because the book still has us
    repeating the function 4 times.
    
    This is rather silly.  =)

--- ch6/dogbark.py
+++ ch6/dogbark.py
@@ -3,9 +3,11 @@
 dog_names = ["Codie", "Sparky", "Jackson", "Fido"]
 dog_weights = [40, 9, 12, 65]
 
-for i in range(len(dog_names)):
-     if dog_weights[i] > 20:
-         print(dog_names[i], 'says WOOF WOOF')
-     else:
-         print(dog_names[i], 'says woof woof')
+def bark(name, weight):
+    if dog_weights[i] > 20:
+        print(dog_names[i], 'says WOOF WOOF')
+    else:
+        print(dog_names[i], 'says woof woof')
 
+for i in range(len(dog_names)):
+     bark(dog_names[i], dog_weights[i])
